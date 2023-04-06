42dc52b5298d9b13be73f2d56d0f1660efef1216
Anastasiia <nastiasereda.1994@gmail.com>
2023-04-06 16:15:20 +0200

    Added prefix for authorization and changed the route

--- frontend/src/api/auth/auth.js
+++ frontend/src/api/auth/auth.js
@@ -1,7 +1,7 @@
 import { api } from '../api';
 
 export const login = (data) => {
-  return api.post('/auth/login', data).then(({ data }) => {
+  return api.post('/users/login', data).then(({ data }) => {
     return data;
   }).catch((data) => {
     throw data;
